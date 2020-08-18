b88165253a094f22d5df5d1fd0f990f847a9e6f0
Adogamm <1718110396@utectulancingo.edu.mx>
2020-08-17 21:46:13 -0500

    Listo para el deploy

--- database/conection.py
+++ database/conection.py
@@ -4,11 +4,11 @@ class Personas():
     def connect(self):
         try:
             self.cnx = mysql.connector.connect(
-                user = 'edvpgj5wh9zmqczv',
-                password = 'odgxk9yd3ng4405u',
-                host = 'g8r9w9tmspbwmsyo.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+                user = 'bs86kst65pkeji8k',
+                password = 'tsmtjvodrckwzxn3',
+                host = 'muowdopceqgxjn2b.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
                 port = 3306,
-                database = 'yjk0glndkjvfo3jg',
+                database = 'sdokaomu5qprynaj',
                 auth_plugin='mysql_native_password'
             )
             self.cursor = self .cnx.cursor(buffered=True)
